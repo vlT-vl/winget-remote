a485a62ad3bb7857472ae22227922c40bf79a15e
Lorenzo Veronesi <vlt.vltech@gmail.com>
2025-02-07 14:21:37 +0100

    Add generateindex.ps1

---
+++ generateindex.ps1
@@ -0,0 +1,38 @@
+$manifestBaseUrl = "https://raw.githubusercontent.com/vlT-vl/winget-remote/refs/heads/main/manifest/"
+
+# Funzione per generare l'index
+function Generate-Index {
+    param (
+        [string]$manifestDirectory,
+        [string]$outputFile
+    )
+
+    $index = @()
+    $manifestLastUpdate = Get-Date -Format "dd/MM/yyyy HH:mm:ss"
+
+    # Recupera tutti i file YAML nella directory
+    $files = Get-ChildItem -Path $manifestDirectory -Filter "*.yaml"
+
+    foreach ($file in $files) {
+        $manifest = Get-Content -Path $file.FullName | ConvertFrom-Yaml
+        $manifestUrl = "$manifestBaseUrl$($file.Name)"
+
+        $packageInfo = @{
+            "Id" = $manifest.Id
+            "Name" = $manifest.Name
+            "Version" = $manifest.Version
+            "Publisher" = $manifest.Publisher
+            "ManifestRelativePath" = $file.FullName.Substring((Get-Location).Path.Length + 1)
+            "ManifestURL" = $manifestUrl
+        }
+
+        $index += $packageInfo
+    }
+
+    # Converti l'array in JSON e scrivilo su file
+    $outputData = @{ "ManifestLastUpdate" = $manifestLastUpdate; "Packages" = $index }
+    $outputData | ConvertTo-Json -Depth 2 | Set-Content -Path $outputFile
+}
+
+# Esegui la funzione con i percorsi richiesti
+Generate-Index -manifestDirectory "./manifest" -outputFile "./manifest/.index.json"
